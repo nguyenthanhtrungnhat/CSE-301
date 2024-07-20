0c78a36ea3faae2b3703645c61001e5e5c6a2622
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-07-20 14:12:41 +0700

    Update SaleManagerment.sql

--- Lab3/SaleManagerment.sql
+++ Lab3/SaleManagerment.sql
@@ -79,7 +79,31 @@ insert into `product` values
 ('P1006','Mouse',5,10,100,105),
 ('P1007','Keyboard',45,60,120,90),
 ('P1008','Headset',63,75,50,40);
+insert into `Salesman` values
+('S001','Huu','Phu Tan','Ho Chi Minh',700002,'Ho Chi Minh',15000,50,35,'0902361123'),
+('S002','Phat','Tan An','Hanoi',700005,'Hanoi',25000,100,110,'0903216542'),
+('S003','Khoa','Phu Hoa','Thu Dau Mot',700051,'Binh Duong',17500,40,30,'0904589632'),
+('S004','Tien','Phu Hoa','Dai An',700023,'Binh Duong',16500,70,72,'0908654723'),
+('S005','Deb','Hoa Phu','Thu Dau Mot',700051,'Binh Duong',13500,60,48,'0903213659'),
+('S006','Tin','Chanh My','Da Lat',700032,'Lam Dong',20000,80,55,'0907853497');
+insert into SalesOrder (order_number,order_date, client_number, salesman_number,delivery_status, delivery_date,order_status)
+values('O20001','2022-01-15','C101','S003','Delivered','2022-02-10','Successful'),
+('O20002','2022-01-25','C102','S003','Delivered','2022-02-15','Cancelled'),
+('O20003','2022-01-31','C103','S002','Delivered','2022-04-03','Successful'),
+('O20004','2022-02-10','C104','S003','Delivered','2022-04-23','Successful'),
+('O20005','2022-02-18','C101','S003','On Way',null,'Cancelled'),
+('O20006','2022-02-22','C105','S005','Ready to Ship',null,'In Process'),
+('O20007','2022-04-03','C106','S001','Delivered','2022-05-08','Successful'),
+('O20008','2022-04-16','C102','S006','Ready to Ship',null,'In Process'),
+('O20009','2022-04-24','C101','S004','On Way',null,'Successful'),
+('O20010','2022-04-29','C106','S006','Delivered','2022-05-08','Successful'),
+('O20011','2022-05-08','C107','S005','Ready to Ship',null,'Cancelled'),
+('O20012','2022-05-12','C108','S004','On Way',null,'Successful'),
+('O20013','2022-05-16','C109','S001','Ready to Ship',null,'In Process'),
+('O20014','2022-05-16','C110','S001','On Way',null,'Successful');
+select * from `SalesOrder`;
 
 
+select * from clients;
 
 
