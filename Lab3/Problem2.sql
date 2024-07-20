418a0976004f6127ad17813e25fd5ab4e032d62a
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-07-20 15:39:45 +0700

    Update Problem2.sql

--- Lab3/Problem2.sql
+++ Lab3/Problem2.sql
@@ -51,7 +51,6 @@ ADD  FOREIGN KEY (departmentID) REFERENCES DEPARTMENT(departmentID);
 
 ALTER TABLE DEPARTMENT
 ADD FOREIGN KEY (managerID) REFERENCES EMPLOYEES(employeeID);
-#alter table EMPLOYEES drop foreign key ;
 
 ALTER TABLE DEPARTMENTADDRESS
 ADD  FOREIGN KEY (departmentID) REFERENCES DEPARTMENT(departmentID);
@@ -66,6 +65,16 @@ ADD  FOREIGN KEY (projectID) REFERENCES PROJECTS(projectID);
 ALTER TABLE RELATIVE
 ADD  FOREIGN KEY (employeeID) REFERENCES EMPLOYEES(employeeID);
 
-insert into EMPLOYEES values(
+-- Insert data into EMPLOYEES table
+INSERT INTO EMPLOYEES (
+    employeeID, lastName, middleName, firstName, dateOfBirth, gender, salary, address, managerID, departmentID
+) VALUES
+('123', 'Dinh', 'Ba', 'Tien', '1995-01-09', 'Nam', 30000.00, '731 Tran Hung Dao Q1 TPHCM', '333', 5),
+('333', 'Nguyen', 'Thanh', 'Tung', '1945-12-08', 'Nam', 40000.00, '638 Nguyen Van Cu QS TPHCM', '888', 5),
+('453', 'Tran', 'Thanh', 'Tam', '1962-07-31', 'Nam', 25000.00, '543 Mai Thi Luu Ba Dinh Ha Noi', '333', 5),
+('666', 'Nguyen', 'Manh', 'Hung', '1952-09-15', 'Nam', 38000.00, '975 Le Lai P3 Vung Tau', '333', 5),
+('777', 'Tran', 'Hong', 'Quang', '1959-03-29', 'Nam', 25000.00, '980 Le Hong Phong Vung Tau', '987', 4),
+('888', 'Vuong', 'Ngoc', 'Quyen', '1927-10-10', 'Nu', 55000.00, '450 Trung Vuong My Tho TG', NULL, 1),
+('957', 'Le', 'Thi', 'Nban', '1931-06-20', 'Nu', 43000.00, '291 Ho Van Hue QPN TPHCM', '588', 1),
+('999', 'Bui', 'Thuy', 'Vu', '1958-07-19', 'Nam', 25000.00, '332 Nguyen Thai Hoe Quy Nhon', '987', 4);
 
-);
